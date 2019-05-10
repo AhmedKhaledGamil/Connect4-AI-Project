a8712a1980b8e679a717c614bcfffc15691ff493
Ahmed Khaled <ahmedgamil10@gmail.com>
2019-05-10 23:59:16 +0200

    Uploading the project
    Created the menu scene
    Made the change scenes function

---
+++ Connect4-AI/src/sample/menuController.java
@@ -0,0 +1,45 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class menuController
+{
+
+    public RadioButton ai_id;
+    public RadioButton player_id;
+
+//    public void startNewGame()
+//    {
+//        if(ai_id.isSelected()){
+//            System.out.println("AI Starts First!");
+//
+//        }
+//        if(player_id.isSelected()){
+//            System.out.println("Player Starts First!");
+//        }
+//    }
+
+    /*
+     *  When this method is called it will change the scene to the board scene!
+     * */
+
+    public void changeSceneToBoard(ActionEvent event) throws IOException {
+        Parent board = FXMLLoader.load(getClass().getResource("board.fxml"));
+        Scene boardScene = new Scene(board, 600, 600);
+
+        // This line gets the stage information
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(boardScene);
+        window.show();
+    }
+}
